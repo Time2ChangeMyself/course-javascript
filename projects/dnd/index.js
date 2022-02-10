76b9e76771e21748e493468d8d010fc787dac1ef
Oleg Denisenko <denisenkoolegq@gmail.com>
2022-02-10 23:28:20 +0700

    dnd homework, first try

--- projects/dnd/index.js
+++ projects/dnd/index.js
@@ -19,9 +19,47 @@ import './dnd.html';
 
 const homeworkContainer = document.querySelector('#app');
 
-document.addEventListener('mousemove', (e) => {});
+document.addEventListener('mousedown', (e) => {
+  document.addEventListener('dragstart', (e) => {
+    e.preventDefault();
+  });
 
-export function createDiv() {}
+  function moveAt(pageX, pageY) {
+    e.target.style.left = pageX - e.offsetX + 'px';
+    e.target.style.top = pageY - e.offsetY + 'px';
+  }
+
+  moveAt(e.pageX, e.pageY);
+
+  function onMouseMove(e) {
+    moveAt(e.pageX, e.pageY);
+  }
+
+  document.addEventListener('mousemove', onMouseMove);
+
+  function mouseUp(e) {
+    document.removeEventListener('mousemove', onMouseMove);
+    document.removeEventListener('mousup', mouseUp);
+  }
+
+  document.addEventListener('mouseup', mouseUp);
+});
+
+export function createDiv() {
+  const newDiv = document.createElement('div');
+  newDiv.classList.add('draggable-div');
+  newDiv.draggable = true;
+  newDiv.style.cssText = `
+  width: ${Math.random() * 100}px; 
+  background-color: #${(Math.random().toString(16) + '000000')
+    .substring(2, 8)
+    .toUpperCase()};
+  height:${Math.random() * 100}px;
+  top:${Math.random() * 1000}px;
+  left:${Math.random() * 1000}px;
+  `;
+  return newDiv;
+}
 
 const addDivButton = homeworkContainer.querySelector('#addDiv');
 
